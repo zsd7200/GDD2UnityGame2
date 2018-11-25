0a76a96adda3a257092c1e8eaffe6139f2aa3cb3
Zack D <zsd7200@rit.edu>
2018-11-25 00:04:51 -0500

    add in floor and crosshair, will start working more on gameobject detection later

---
+++ GDD2UnityGame2/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public Texture2D crosshair; // crosshair image
+
+    public RaycastHit hit;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(new Vector3((Screen.width / 2) - (crosshair.width / 2), (Screen.height / 2) - (crosshair.height / 2), 0));
+
+        //if (Physics.Raycast(ray, out hit, 10))
+        //{
+        //    Debug.Log("touched book");
+        //}
+    }
+
+    // draw crosshair
+    void OnGUI()
+    {
+        // create style to have no border/bg
+        GUIStyle style = new GUIStyle();
+        style.border = new RectOffset(0, 0, 0, 0);
+
+        // draw crosshair
+        GUI.Box(new Rect((Screen.width / 2) - (crosshair.width / 2), (Screen.height / 2) - (crosshair.height / 2), crosshair.width, crosshair.height), crosshair, style);
+    }
+}
