dcc630bb85e3fdb7bbccbd1dd16638d48e23bfab
Zack D <zsd7200@rit.edu>
2018-11-26 01:34:37 -0500

    make the room sorta

---
+++ GDD2UnityGame2/Assets/Scripts/SpinFan.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinFan : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 0, 1), 2);
+	}
+}
