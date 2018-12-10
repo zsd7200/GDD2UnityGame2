1c2bed16466e6df1871dfa12cc594e1cd207c727
Zack Dunham (RIT Student) <zsd7200@ad.rit.edu>
2018-12-10 10:40:29 -0500

    add in pause menu

---
+++ GDD2UnityGame2/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseUI;
+    public GameObject fpsController;
+
+    private void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (pauseUI.activeSelf == true)
+                Resume();
+            else
+                Pause();
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        fpsController.GetComponent<FirstPersonController>().enabled = true;
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        fpsController.GetComponent<FirstPersonController>().enabled = false;
+        Time.timeScale = 0f;
+    }
+
+    public void Restart()
+    {
+        Resume();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu()
+    {
+        Resume();
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
