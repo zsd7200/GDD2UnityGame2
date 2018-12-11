f437409b2213c9ff4ed1d181fbd18e2c3ccfde19
pjh9488 <pjh9488@IGM2025-17>
2018-12-11 16:34:48 -0500

    Changed Lighting and candles

---
+++ GDD2UnityGame2/Assets/Scripts/LightCandle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCandle : MonoBehaviour {
+
+    GameObject child;
+    GameObject thisObject;
+
+	// Use this for initialization
+	void Start () {
+        thisObject = this.gameObject; //Current Object
+        child = thisObject.gameObject.transform.GetChild(0).gameObject; //Child of object
+        child.SetActive(false); //Extinguish flame
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LightFlame();
+	}
+
+    void LightFlame()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) //If the E key is down
+        {
+            child.SetActive(true); //Light the Flame
+        }
+    }
+
+}
