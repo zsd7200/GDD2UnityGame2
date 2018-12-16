49d6ad67884131288819292096e459116def1f6e
Nick Visconti (RIT Student) <njv7168@ad.rit.edu>
2018-12-15 21:34:30 -0500

    Making animation work

---
+++ GDD2UnityGame2/Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+    public Animator anim;
+    public Rigidbody rBody;
+
+    private float inputH;
+    private float inputV;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+
+    }
+}
